b00e87460c736d723388a06e68a50e256d4f954c
paolo.caligiana <paolo.caligiana@vection.com.au>
2025-11-20 14:09:25 +0000

    🧹 chore: clean up unused api key, vite proxy server, env variables

--- vite.config.ts
+++ vite.config.ts
@@ -2,18 +2,7 @@ import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react-swc";
 
-const BASE_URL = "https://api.musixmatch.com/ws/1.1";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  server: {
-    proxy: {
-      "/musix": {
-        target: BASE_URL,
-        changeOrigin: true,
-        secure: false,
-        rewrite: (path) => path.replace(/^\/musix/, ""),
-      },
-    },
-  },
 });
